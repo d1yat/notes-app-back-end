4cf2e695b6001ad701f171e5a1fd36c13d49f61f
d1yat <me.sudiyat@gmail.com>
2021-05-27 05:47:22 +0700

    change host value

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
 	const server = Hapi.server({
 		port: 5000,
-		host: process.env.NODE_ENV === 'development' ? 'localhost' : '172.31.19.146',
+		host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.19.146',
 		routes: {
 			cors: {
 				origin: ['*']
